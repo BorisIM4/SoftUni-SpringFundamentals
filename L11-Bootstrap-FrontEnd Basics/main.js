921badc2c96488d5253a3261739af1603cf120c9
JRB <jrb@abv.bg>
2023-03-13 21:50:56 +0200

    Add bootstrap examples

---
+++ L11-Bootstrap-FrontEnd Basics/main.js
@@ -0,0 +1,26 @@
+num = 6;
+console.log(num)
+var num;
+
+let p1 = {
+    fname: "Bobi",
+    sname: "Milanov"
+}
+
+let p2 = {
+    fname: "Aleks",
+    sname: "Milanova"
+}
+
+let p3 = {
+    fname: "Krisiya",
+    sname: "Milanova"
+}
+
+var persons = ([p1, p2])
+persons.push(p3)
+
+let x = p1.sname
+let y = x
+
+console.log(y)
